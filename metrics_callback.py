e8c1f9b1537f7f8752e12048ae42bf36d75a62ea
ZixiBenZhang <13126817936@163.com>
2023-10-27 08:39:13 +0100

    Added callback logging validation metric history

---
+++ metrics_callback.py
@@ -0,0 +1,19 @@
+import torch
+import pytorch_lightning as pl
+from torch import Tensor
+
+
+class ValidationMetricsCallback(pl.Callback):
+    def __init__(self):
+        super().__init__()
+        self.val_history_metrics: dict[str, list[Tensor]] = {}
+
+    def on_validation_epoch_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+        metrics = trainer.callback_metrics
+        for k in metrics:
+            if "val" not in k:
+                continue
+            if k in self.val_history_metrics:
+                self.val_history_metrics[k].append(metrics[k])
+            else:
+                self.val_history_metrics[k] = [metrics[k]]
