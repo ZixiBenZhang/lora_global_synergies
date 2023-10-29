2cf35d88a9c6e90e65774c48c04151a4738a87ca
ZixiBenZhang <13126817936@163.com>
2023-10-29 16:18:05 +0000

    Updated config for SST2

--- train.sh
+++ train.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-torchrun --nproc_per_node gpu main.py train --config configs/opt_plain_mnli.toml
+export CUDA_VISIBLE_DEVICES=1,3
+torchrun --nproc_per_node 2 main.py train --config configs/opt_plain_sst2.toml \
+--num-devices 2
 # OPT125M + RTE, 40 cpu + 2 gpu: 4min to setup & tokenize data splits before running fit
