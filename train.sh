e136ad1516ba12f9e3dd406326548072c8deab8a
ZixiBenZhang <13126817936@163.com>
2023-11-03 15:54:57 +0000

    Testing MRPC with LR = 1e-6 ~ 1e-5, SST2 with LR=2e-5

--- train.sh
+++ train.sh
@@ -2,5 +2,6 @@
 
 export CUDA_VISIBLE_DEVICES=1,3
 torchrun --nproc_per_node 2 main.py train --config configs/opt_plain_sst2.toml \
---num-devices 2
+--num-devices 2 \
+--learning-rate NUM
 # OPT125M + RTE, 40 cpu + 2 gpu: 4min to setup & tokenize data splits before running fit
