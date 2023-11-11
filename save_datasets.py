8312012cadad0f3cf727647b98ae550fe5aa2fe9
ZixiBenZhang <13126817936@163.com>
2023-11-11 23:32:27 +0000

    Updated save_datasets.py: doesn't save to disk, only download, so copy ~/.cache/huggingface/datasets after then

--- save_datasets.py
+++ save_datasets.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     config_names = datasets.get_dataset_config_names("glue")
     for c in config_names:
         dataset: DatasetDict = datasets.load_dataset("glue", c)
-        dataset.save_to_disk(f"../saved_hf_datasets")
+        # dataset.save_to_disk(f"../saved_hf_datasets/glue/{c}")
