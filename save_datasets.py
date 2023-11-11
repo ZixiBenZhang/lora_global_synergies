3b64eb1e2e3dcf6e03320c37637571c3fb74a4a7
ZixiBenZhang <13126817936@163.com>
2023-11-11 22:04:19 +0000

    Built script for saving GLUE datasets to disk

---
+++ save_datasets.py
@@ -0,0 +1,8 @@
+import datasets
+from datasets import DatasetDict
+
+if __name__ == "__main__":
+    config_names = datasets.get_dataset_config_names("glue")
+    for c in config_names:
+        dataset: DatasetDict = datasets.load_dataset("glue", c)
+        dataset.save_to_disk(f"../saved_hf_datasets")
