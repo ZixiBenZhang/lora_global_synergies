f99d6b179155909cf5e132a12e5ab2e5725c7c2d
ZixiBenZhang <13126817936@163.com>
2023-11-12 16:09:12 +0000

    Updated to download GLUE, XSum, SuperGLUE as well as their DatasetInfo

--- save_datasets.py
+++ save_datasets.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     config_names = datasets.get_dataset_config_names("glue")
     for c in config_names:
         dataset: DatasetDict = datasets.load_dataset("glue", c)
-        info = datasets.get_dataset_config_info("super_glue", c)
+        info = datasets.get_dataset_config_info("glue", c)
 
     dataset = datasets.load_dataset("xsum")
     info = datasets.get_dataset_infos("xsum")["default"]
