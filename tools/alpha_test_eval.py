45c845590a7eebf3f281d949091f439a465f7a7b
ZixiBenZhang <13126817936@163.com>
2024-02-22 20:45:51 +0000

    Debugging

--- tools/alpha_test_eval.py
+++ tools/alpha_test_eval.py
@@ -4,7 +4,7 @@ import toml
 def analyse():
     with open(
         "../ags_output/opt_lora_classification_rte_2024-02-01/checkpoints/logs_test/importance_22-40.toml",
-        "r"
+        "r",
     ) as f:
         data = toml.load(f)
     for mat, res in data.items():
