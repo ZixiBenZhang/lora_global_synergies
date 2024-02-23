47f6695f5c6e7a6f7ff696bbca6070e577385bb4
ZixiBenZhang <13126817936@163.com>
2024-02-23 22:26:46 +0000

    Accommodated to different task types. Built zero-proxy training to train LoRA with 1% dataset

--- actions/alpha_importance_test.py
+++ actions/alpha_importance_test.py
@@ -1,3 +1,4 @@
+import copy
 import logging
 import os
 import pickle
@@ -9,14 +10,17 @@ import pytorch_lightning as pl
 from lightning_fabric.plugins.environments import SLURMEnvironment
 from pytorch_lightning.loggers import TensorBoardLogger, CSVLogger
 
-from lora.lora_modules import LoraLinear
+from lora.lora_modules import LoraLinear, mark_only_lora_as_trainable, update_lora_importance_alpha_require_grad
+from models.model_info import AgsModelInfo
 from models.modeling_opt_lora import (
     OPTLoraForCausalLM,
     OPTLoraForQuestionAnswering,
     OPTLoraForSequenceClassification, OPTLoraDecoderLayer,
 )
+from projectors.shortcut_modules import mark_ags_as_trainable
 from tools.checkpoint_load import load_model_chkpt
 import pl_model_wrapper
+from tools.trainable_param_printer import print_trainable_parameters
 
 logger = logging.getLogger(__name__)
 
@@ -24,71 +28,117 @@ logger = logging.getLogger(__name__)
 def alpha_importance_test(
     model: torch.nn.Module | torch.fx.GraphModule,
     tokenizer,
-    model_info,  # dataclass of model's task type and name
+    model_info: AgsModelInfo,  # dataclass of model's task type and name
     data_module: pl.LightningDataModule,  # for preparing and loading datasets for pl trainer
     dataset_info,  # dataclass including e.g. number of classes for the pl model wrapper
     task,  # to decide the pl model wrapper of which type should be used
     optimizer,  # optimizer for pl trainer
     learning_rate,  # lr for optimizer. lr_scheduler is default as CosineAnnealingLR
     weight_decay,  # weight_decay for optimizer
+    lr_scheduler,  # for building lr scheduler
+    eta_min,  # for building lr scheduler
     pl_trainer_args,  # args for pl trainer; include e.g. "max_epochs" for setting up lr_scheduler
     auto_requeue,  # for setting up SLURMEnvironment, environment for distributed launch
     save_path,  # path for saving checkpoints
     load_name,  # path to the saved checkpoint
     load_type,  # model checkpoint's type: ['pt', 'pl']
+    resume_training,  # whether resume zero-proxy trained model from the checkpoint
 ):
     t = time.strftime("%H-%M")
 
-    if save_path is not None:
-        if not os.path.isdir(save_path):
-            os.makedirs(save_path)
-        # tb_logger = TensorBoardLogger(save_dir=save_path, name="logs_test")
-        # csv_logger = CSVLogger(save_dir=save_path, name="alpha_importance_logs")
-        pl_trainer_args["callbacks"] = []
-        pl_trainer_args["logger"] = []
+    pl_validator_args = copy.deepcopy(pl_trainer_args)
 
-    if auto_requeue is not None:
-        plugins = [SLURMEnvironment(auto_requeue=auto_requeue)]
-    else:
-        plugins = None
-    pl_trainer_args["plugins"] = plugins
-
-    wrapper_pl_model: pl.LightningModule = pl_model_wrapper.get_model_wrapper(
-        model_info, task
-    )
-
-    # load model from pl checkpoint
-    if load_name is None:
-        raise ValueError(
-            "Path to checkpoint required for resuming training. Please use --load PATH."
+    if resume_training:
+        wrapper_pl_model: pl.LightningModule = pl_model_wrapper.get_model_wrapper(
+            model_info, task
         )
-    model = load_model_chkpt(load_name, load_type=load_type, model=model)
-    # if load_type != "pl":
-    #     raise ValueError("Load-type pl is required for resuming training. Please use --load-type pl.")
-    logger.warning(
-        f"Restore model state from pl checkpoint {load_name}. Entered hyperparameter configuration ignored."
-    )
 
-    pl_model = wrapper_pl_model.load_from_checkpoint(load_name, model=model)
+        # load model from pl checkpoint
+        if load_name is not None:
+            model = load_model_chkpt(load_name, load_type=load_type, model=model)
+
+        logger.warning(
+            f"Restore model state from pl checkpoint {load_name}. Entered hyperparameter configuration ignored."
+        )
+
+        pl_model = wrapper_pl_model(
+            model,
+            dataset_info=dataset_info,
+            learning_rate=learning_rate,
+            weight_decay=weight_decay,
+            lr_scheduler=lr_scheduler,  # for building lr scheduler
+            eta_min=eta_min,  # for building lr scheduler
+            epochs=pl_trainer_args["max_epochs"],
+            optimizer=optimizer,
+        )
+    else:
+        pl_model = zero_proxy_train_lora(
+            model,
+            tokenizer,
+            model_info,
+            data_module,
+            dataset_info,
+            task,
+            optimizer,
+            learning_rate,
+            weight_decay,
+            lr_scheduler,
+            eta_min,
+            pl_trainer_args,
+            auto_requeue,
+            save_path,
+            load_name,
+            load_type,
+        )
 
     # Run each test only on one minibatch
-    trainer = pl.Trainer(**pl_trainer_args, limit_val_batches=1)
-
-    logger.warning(
-        f"Using perplexity as the testing metrics. Currently only causal LM models supported."
-    )
-    assert task == "causal_language_modeling"
-
+    trainer = pl.Trainer(**pl_validator_args, limit_val_batches=1)
     original_val_metrics = trainer.validate(pl_model, datamodule=data_module)[0]
-    original_perplexity = original_val_metrics["val_perplexity_epoch"]
 
-    threshold_perplexity = original_perplexity + 0.005 * original_perplexity
+    def get_metric_name():
+        match task:
+            case "classification":
+                return "val_acc_epoch"
+            case "summarization":
+                return "val_rouge_epoch"
+            case "causal_language_modeling":
+                return "val_perplexity_epoch"
+            case _:
+                raise ValueError(f"Unsupported task: {task}")
+
+    def get_metric_threshold():
+        original_metric = original_val_metrics[get_metric_name()]
+        match task:
+            case "classification":
+                # Accuracy
+                return original_metric - original_metric * 0.01
+            case "summarization":
+                # Rouge score
+                return original_metric - original_metric * 0.01
+            case "causal_language_modeling":
+                # Perplexity
+                return original_metric + original_metric * 0.01
+            case _:
+                raise ValueError(f"Unsupported task: {task}")
+
+    def check_exceed_threshold(val_metrics_dict):
+        val_metric = val_metrics_dict[get_metric_name()]
+        threshold = get_metric_threshold()
+        match task:
+            case "classification":
+                return val_metric <= threshold
+            case "summarization":
+                return val_metric <= threshold
+            case "causal_language_modeling":
+                return val_metric >= threshold
+            case _:
+                raise ValueError(f"Unsupported task: {task}")
 
     # Result format: {layer_idx: {proj: alpha}}
     res_val: dict[str, str | float | dict[str, float]] = {
         "task": task,
-        "metrics": "perplexity",
-        "original_perplexity": original_perplexity,
+        "metric_name": get_metric_name(),
+        "zero-proxy_metric": original_val_metrics[get_metric_name()],
     }
 
     # FOR RESUMING ALPHA TESTING
@@ -98,12 +148,12 @@ def alpha_importance_test(
     with torch.no_grad():
         assert (
             type(model) is OPTLoraForCausalLM
-            # or type(model) is OPTLoraForSequenceClassification
-            # or type(model) is OPTLoraForQuestionAnswering
+            or type(model) is OPTLoraForSequenceClassification
+            or type(model) is OPTLoraForQuestionAnswering
         )
-        model: OPTLoraForCausalLM  # | OPTLoraForSequenceClassification | OPTLoraForQuestionAnswering
+        model: OPTLoraForCausalLM | OPTLoraForSequenceClassification | OPTLoraForQuestionAnswering
 
-        # Resume alpha testing
+        # RESUME ALPHA TESTING
         if resume_layer_id > 0:
             with open(resume_toml, "r") as f:
                 res_val = toml.load(f)
@@ -119,6 +169,7 @@ def alpha_importance_test(
             decoder_layer: OPTLoraDecoderLayer
             layer_id = decoder_layer.layer_id
 
+            # FOR RESUMING ALPHA TESTING
             if layer_id < resume_layer_id:
                 continue
 
@@ -139,15 +190,13 @@ def alpha_importance_test(
                 alpha = 0.5
                 lora.importance_alpha[adapter_name] = alpha
                 val_metrics = trainer.validate(pl_model, datamodule=data_module)[0]
-                perplexity = val_metrics["val_perplexity_epoch"]
 
-                if perplexity >= threshold_perplexity:
+                if check_exceed_threshold(val_metrics):
                     while alpha < 1.0:
                         alpha += 0.1
                         lora.importance_alpha[adapter_name] = alpha
                         val_metrics = trainer.validate(pl_model, datamodule=data_module)[0]
-                        perplexity = val_metrics["val_perplexity_epoch"]
-                        if perplexity < threshold_perplexity:
+                        if not check_exceed_threshold(val_metrics):
                             alpha_res = alpha - 0.1
                             break
                 else:
@@ -155,8 +204,7 @@ def alpha_importance_test(
                         alpha -= 0.1
                         lora.importance_alpha[adapter_name] = alpha
                         val_metrics = trainer.validate(pl_model, datamodule=data_module)[0]
-                        perplexity = val_metrics["val_perplexity_epoch"]
-                        if perplexity >= threshold_perplexity:
+                        if check_exceed_threshold(val_metrics):
                             alpha_res = alpha
                             break
 
@@ -164,3 +212,83 @@ def alpha_importance_test(
                 save_toml(res_val)
 
         save_toml(res_val)
+
+
+def zero_proxy_train_lora(
+    model: torch.nn.Module | torch.fx.GraphModule,
+    tokenizer,
+    model_info: AgsModelInfo,  # dataclass of model's task type and name
+    data_module: pl.LightningDataModule,  # for preparing and loading datasets for pl trainer
+    dataset_info,  # dataclass including e.g. number of classes for the pl model wrapper
+    task,  # to decide the pl model wrapper of which type should be used
+    optimizer,  # optimizer for pl trainer
+    learning_rate,  # lr for optimizer. lr_scheduler is default as CosineAnnealingLR
+    weight_decay,  # weight_decay for optimizer
+    lr_scheduler,  # for building lr scheduler
+    eta_min,  # for building lr scheduler
+    pl_trainer_args,  # args for pl trainer; include e.g. "max_epochs" for setting up lr_scheduler
+    auto_requeue,  # for setting up SLURMEnvironment, environment for distributed launch
+    save_path,  # path for saving checkpoints
+    load_name,  # path to the saved checkpoint
+    load_type,  # model checkpoint's type: ['pt', 'pl']
+):
+    if save_path is not None:
+        if not os.path.isdir(save_path):
+            os.makedirs(save_path)
+        latest_checkpoint_callback = pl.callbacks.ModelCheckpoint(
+            dirpath=save_path,
+            filename="alpha-zero-proxy_last_chkpt",
+            # save_last=True,
+        )
+        # Monitoring lr for the lr_scheduler
+        lr_monitor_callback = pl.callbacks.LearningRateMonitor(logging_interval="step")
+        # TensorBoard logger
+        tb_logger = TensorBoardLogger(save_dir=save_path, name="alpha-testing_logs")
+        pl_trainer_args["callbacks"] = [
+            latest_checkpoint_callback,
+            lr_monitor_callback,
+        ]
+        pl_trainer_args["logger"] = [tb_logger]
+
+    if auto_requeue is not None:
+        plugins = [SLURMEnvironment(auto_requeue=auto_requeue)]
+    else:
+        plugins = None
+    pl_trainer_args["plugins"] = plugins
+
+    wrapper_pl_model: pl.LightningModule = pl_model_wrapper.get_model_wrapper(
+        model_info, task
+    )
+
+    # load model state from checkpoint
+    if load_name is not None:
+        model = load_model_chkpt(load_name, load_type=load_type, model=model)
+        logger.warning(
+            f"Restore model state from pl checkpoint {load_name}. Entered model hyperparameter configuration ignored."
+        )
+
+    pl_model: pl.LightningModule = wrapper_pl_model(
+        model,
+        dataset_info=dataset_info,
+        learning_rate=learning_rate,
+        weight_decay=weight_decay,
+        lr_scheduler=lr_scheduler,  # for building lr scheduler
+        eta_min=eta_min,  # for building lr scheduler
+        epochs=pl_trainer_args["max_epochs"],
+        optimizer=optimizer,
+    )
+
+    if model_info.is_lora:
+        mark_only_lora_as_trainable(model, bias="none")
+        update_lora_importance_alpha_require_grad(model, require_grad=False)
+
+        if model_info.is_ags:
+            mark_ags_as_trainable(model)
+        print_trainable_parameters(model)
+
+    # Zero-proxy training for LoRA modules
+    trainer = pl.Trainer(**pl_trainer_args, limit_train_batches=0.01)
+
+    trainer.fit(pl_model, datamodule=data_module)
+
+    return pl_model
