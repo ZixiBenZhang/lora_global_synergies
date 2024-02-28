94276c5735c6496bb52be4ea537f57b269237c79
ZixiBenZhang <13126817936@163.com>
2024-02-28 20:15:31 +0000

    Debugging: lm_head substituted outside __init__

--- models/modeling_opt_lora.py
+++ models/modeling_opt_lora.py
@@ -988,11 +988,11 @@ class OPTLoraForCausalLM(OPTLoraPreTrainedModel):
     def set_input_embeddings(self, value):
         self.model.decoder.embed_tokens = value
 
-    # def get_output_embeddings(self):
-    #     return self.lm_head
-    #
-    # def set_output_embeddings(self, new_embeddings):
-    #     self.lm_head = new_embeddings
+    def get_output_embeddings(self):
+        return self.lm_head
+
+    def set_output_embeddings(self, new_embeddings):
+        self.lm_head = new_embeddings
 
     def set_decoder(self, decoder):
         self.model.decoder = decoder
