8853c9a82df555cc4d63ac5c2a298a23df2b6955
ZixiBenZhang <13126817936@163.com>
2024-03-22 21:33:52 +0000

    Debugging

--- importance_testing/synflow.py
+++ importance_testing/synflow.py
@@ -116,10 +116,15 @@ def synflow_test(
     # compute gradients with input of ones
     pl_model.to("cuda")
     pl_model.zero_grad()
-    example_input = next(iter(dataloader))["input_ids"]
-    input_dim = list(example_input.shape)
+    example_input = next(iter(dataloader))
+    input_dim = list(example_input["input_ids"].shape)
     inputs = torch.ones([1] + input_dim).double().to("cuda")
-    output = pl_model.forward(inputs)
+    output = pl_model.forward(
+        inputs,
+        example_input["attention_mask"],
+        example_input.get("token_type_ids", None),
+        example_input["labels"],
+    )
     torch.sum(output).backward()
 
     # calculate score of every lora module
