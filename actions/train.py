9cb12f8b3d2f16465e18e3399a9c02edab526610
ZixiBenZhang <13126817936@163.com>
2024-04-26 14:35:23 +0100

    Added seed logging in tensorboard

--- actions/train.py
+++ actions/train.py
@@ -71,7 +71,7 @@ def train(
         # TensorBoard logger
         tb_logger = pl.loggers.TensorBoardLogger(save_dir=save_path, name="logs")
         tb_logger.log_hyperparams(ags_config_paths)
-        tb_logger.log_hyperparams(seed)
+        tb_logger.log_hyperparams({"seed": seed})
         # csv_logger = pl.loggers.CSVLogger(save_dir=save_path, name="csv_logs")
         # wandb_logger = pl.loggers.WandbLogger(save_dir=save_path, name="wandb_logs")
         pl_trainer_args["callbacks"] = [
