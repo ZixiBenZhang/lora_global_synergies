dfff0a01271d76d62928de37b05d663775b8a7c5
ZixiBenZhang <13126817936@163.com>
2024-07-25 20:15:21 +0100

    Debugging Llama Lora

--- models/modeling_llama_lora.py
+++ models/modeling_llama_lora.py
@@ -479,7 +479,6 @@ class LlamaLoraDecoderLayer(nn.Module):
             output_attentions=output_attentions,
             use_cache=use_cache,
             cache_position=cache_position,
-            position_embeddings=position_embeddings,
             **kwargs,
         )
         hidden_states = residual + hidden_states
