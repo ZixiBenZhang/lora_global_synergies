5fc3289a1dbf398ee72bda344358dc40469ea752
ZixiBenZhang <13126817936@163.com>
2024-08-01 21:43:09 +0100

    Updated LlamaLoRA config: using pad_token=eos_token as default

--- models/configuration_llama_lora.py
+++ models/configuration_llama_lora.py
@@ -134,7 +134,7 @@ class LlamaLoraConfig(PretrainedConfig):
         initializer_range=0.02,
         rms_norm_eps=1e-6,
         use_cache=True,
-        pad_token_id=None,
+        pad_token_id=2,
         bos_token_id=1,
         eos_token_id=2,
         pretraining_tp=1,
